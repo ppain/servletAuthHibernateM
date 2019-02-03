33a59c3b347fcbc8838b064ef021e398bbdad23b
ppain <etomoemail@ya.ru>
2019-02-03 14:21:44 +0700

    add servlet

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,14 +1,42 @@
 package main;
 
 
+import accounts.AccountService;
+import accounts.UserProfile;
 import dbService.DBException;
 import dbService.DBService;
 import dbService.dataSets.UsersDataSet;
+import org.eclipse.jetty.server.Handler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+import servlets.SignInServlet;
+import servlets.SignUpServlet;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         DBService dbService = new DBService();
         dbService.printConnectInfo();
+
+        AccountService accountService = new AccountService();
+
+        accountService.addNewUser(new UserProfile("admin"));
+        accountService.addNewUser(new UserProfile("test"));
+
+        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(new SignUpServlet(accountService)), "/signup");
+        context.addServlet(new ServletHolder(new SignInServlet(accountService)), "/signin");
+
+        ResourceHandler resource_handler = new ResourceHandler();
+        resource_handler.setResourceBase("src\\main\\resources\\");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[]{resource_handler, context});
+
+        Server server = new Server(8080);
+        server.setHandler(handlers);
 
         try {
             long userId = dbService.addUser("testovsky");
@@ -20,5 +48,10 @@ public class Main {
         } catch (DBException e) {
             e.printStackTrace();
         }
+
+        server.start();
+        java.util.logging.Logger.getGlobal().info("Server started");
+        server.join();
+
     }
 }
---
+++ src/main/java/servlets/SignUpServlet.java
@@ -0,0 +1,48 @@
+package servlets;
+
+import accounts.AccountService;
+import accounts.UserProfile;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+public class SignUpServlet extends HttpServlet {
+
+    private final AccountService accountService;
+
+    public SignUpServlet(AccountService accountService) {
+        this.accountService = accountService;
+    }
+
+//    //get public user profile
+//    public void doGet(HttpServletRequest request,
+//                      HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+
+    //sign up
+    public void doPost(HttpServletRequest request,
+                       HttpServletResponse response) throws ServletException, IOException {
+        String login = request.getParameter("login");
+        String pass = request.getParameter("password");
+        accountService.addNewUser(new UserProfile(login,pass,login));
+        response.setContentType("text/html;charset=utf-8");
+        response.setStatus(HttpServletResponse.SC_OK);
+    }
+
+    //change profile
+    public void doPut(HttpServletRequest request,
+                      HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    //unregister
+    public void doDelete(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
